e638a3db56ea86784fe422623a8b5cd0a295643b
Lauren Gayle <laurenegayle22@gmail.com>
2024-07-24 18:05:50 +0000

    xrdp

--- .devcontainer/container_post_start.sh
+++ .devcontainer/container_post_start.sh
@@ -13,3 +13,27 @@ if [[ "$HOST_OS" == "darwin" ]] && [[ -n "$HOST_DISPLAY" ]]; then
   echo "Starting virtual display at :99 ..."
   tmux new-session -d -s fakedisplay Xvfb :99 -screen 0 1920x1080x24
 fi
+
+#https://github.com/danchitnis/container-xrdp/blob/master/build/ubuntu-run.sh
+
+start_xrdp_services() {
+    # Preventing xrdp startup failure
+    rm -rf /var/run/xrdp-sesman.pid
+    rm -rf /var/run/xrdp.pid
+    rm -rf /var/run/xrdp/xrdp-sesman.pid
+    rm -rf /var/run/xrdp/xrdp.pid
+
+    # Use exec ... to forward SIGNAL to child processes
+    xrdp-sesman && exec xrdp -n
+}
+
+stop_xrdp_services() {
+    xrdp --kill
+    xrdp-sesman --kill
+    exit 0
+}
+
+echo -e "starting xrdp services...\n"
+
+trap "stop_xrdp_services" SIGKILL SIGTERM SIGHUP SIGINT EXIT
+start_xrdp_services
