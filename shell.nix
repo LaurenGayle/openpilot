b90cc0c97a2a27e35b92ed944463acb81770e83d
Lauren Gayle <laurenegayle22@gmail.com>
2025-02-19 01:50:24 -0500

    Messy nix shell; cannot find c headers for capnproto

--- shell.nix
+++ shell.nix
@@ -1,80 +1,53 @@
-# simple.nix
-with (import <nixpkgs> {});
-mkShell {
-  buildInputs = [
-    clang
-    cppcheck
-    gnumake pkgsCross.muslpi.buildPackages.gcc
-    capnproto 
-    ocamlPackages.capnp
-    #python311Packages.pycapnp
-    emacsPackages.capnp-mode
-    curl 
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  name = "cpp_project";
+  buildInputs = with pkgs; [
+    cmake
+    ninja
+    swig
+    pcre
+    bison
+    flex
     openssl
-    git 
-    git-lfs 
-    ffmpeg 
-    #libavformat-dev 
-    ocamlPackages.ffmpeg-avcodec
-    ocamlPackages.ffmpeg-avdevice
-    ocamlPackages.ffmpeg-avutil
-    ocamlPackages.ffmpeg-avfilter 
-    python312Packages.bz2file 
-    ocamlPackages.eigen 
-    #libffi-dev 
-    glew 
-    mesa
-    chickenPackages_5.chickenEggs.glfw3 #libglfw3-dev 
-    libglibutil
-    libsForQt5.qt5ct #libqt5charts5-dev 
-    ncurses5
-    libhv #libssl-dev 
-    libusbp
-    czmq #libzmq3-dev 
-    sqlite #libsqlite3-dev
-    systemdLibs
-    glibcLocales 
-    opencl-headers
-    ocl-icd #ocl-icd-libopencl1 
-    #ocl-icd-opencl-dev 
-    portaudio# portaudio19-dev 
-    libsForQt5.qt5.qtmultimedia 
-    libsForQt5.qt5.qtdeclarative 
-    libsForQt5.qt5.qttools 
-    libsForQt5.qt5.qtsvg
-    libsForQt5.qt5.qtserialbus  
-    libsForQt5.qt5.qtx11extras 
-    savvycan
-    xorg_sys_opengl #libqt5opengl5-dev
-    casync 
-    cmake 
-    gnumake #make 
-    clinfo 
-    vtkWithQt5 #libqt5sql5-sqlite 
-    readline
-    libdwarf-lite 
-    autoconf 
-    libtool 
-    bzip2
-    #libarchive-dev 
-    #libncursesw5-dev 
-    #libportaudio2 
-    #locales
-    libgcc
-    libsForQt5.qt5.qtbase
-    #qtchooser 
-    libsForQt5.qmake
-    #qtbase5-dev-tools 
-    python3Full #python3-dev 
-    #python312Packages.venvShellHook #python3-venv
-    #libavresample-dev
-    #qt5-default 
-    #python-dev
-    python312Packages.pydevtool
-    scons
-    poetry
+
+    cereal
+    cppcheck
+    clang
+    capnproto
     libsForQt5.full
-    kdePackages.full
-    docker
+    gcc-arm-embedded
+    libgcc
+    libarchive
+    zeromq
+    zmqpp
+    czmq
+    llvmPackages_latest.llvm
+
+    ffmpeg
+    glew
+    mesa
+    chickenPackages_5.chickenEggs.glfw3
+    libglibutil
+    ncurses5
+    libhv
+    libusbp
+    sqlite
+    systemdLibs
+    glibcLocales
+    opencl-headers
+    portaudio
+    bzip2
   ];
+
+  nativeBuildInputs = [ pkgs.pkg-config ];
+
+  shellHook = ''
+    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$HOME/project/libmanolo/";
+    export CMAKE_INCLUDE_PATH="$usr/local/include/":$CMAKE_INCLUDE_PATH;
+    export CPLUS_INCLUDE_PATH="$usr/local/include/":$CPLUS_INCLUDE_PATH;
+    export  NIX_LDFLAGS="-L${stdenv.cc.cc}/lib/gcc/${targetPlatform.config}/${stdenv.cc.cc.version}";
+    export  CFLAGS="-B${stdenv.cc.cc}/lib/gcc/${targetPlatform.config}/${stdenv.cc.cc.version} -B ${stdenv.cc.libc}/lib";
+    echo "Starting new shell";
+  '';
 }
